cfa58992f30dd7b7fe48bc84a1da35ef7a940fa7
Muhammad Ahmed <ahmed.hanjra96@gmail.com>
2022-11-17 10:53:56 +0500

    quoes page done with hotwire and turbo

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: "pages#home"
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
+end
